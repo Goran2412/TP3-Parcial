8318fbc967abcc8b7a24f8facfc44bf8e29c5144
LucasG <granellilucas@gmail.com>
2023-10-31 21:35:08 -0300

    sdk fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.parcialtp3"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.parcialtp3"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
