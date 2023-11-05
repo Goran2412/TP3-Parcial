5dcd3840272729c68ea136bd01cd1ccf8a1c6ca1
ezequieelq <ezequieelquevedo@gmail.com>
2023-11-05 04:34:31 -0300

    searchbar in appbar

--- app/src/main/java/com/example/parcialtp3/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/parcialtp3/ui/home/HomeFragment.kt
@@ -1,12 +1,21 @@
 package com.example.parcialtp3.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.parcialtp3.R
 import com.example.parcialtp3.databinding.FragmentHomeBinding
 //import dagger.hilt.android.AndroidEntryPoint
@@ -42,8 +51,39 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupMenu()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) {
+            }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+
+                menuInflater.inflate(R.menu.search_menu, menu)
+
+                val menuItem = menu.findItem(R.id.search)
+                val searchView = menuItem.actionView as SearchView
+                searchView.queryHint = "Buscá por raza o ubicación.."
+
+                searchView.setOnSearchClickListener {
+                    //TODOfindNavController().navigate(R.id.action_homeListFragment_to_searchFragment)
+                }
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                Log.d("MenuHost", "onMenuItemSelected executed")
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED).also {
+        }
     }
 }
