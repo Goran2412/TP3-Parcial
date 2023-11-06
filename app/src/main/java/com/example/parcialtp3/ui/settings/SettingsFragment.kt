92df739ec9acf868cce18bc9c937cea0d63d3ce6
LucasG <granellilucas@gmail.com>
2023-11-06 19:49:31 -0300

    darkMode switch implemented

--- app/src/main/java/com/example/parcialtp3/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/parcialtp3/ui/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.parcialtp3.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.viewModels
-import androidx.preference.CheckBoxPreference
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.example.parcialtp3.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,12 +18,28 @@ class SettingsFragment : PreferenceFragmentCompat() {
         Log.d(TAG2, "Init settings fragment")
     }
 
-
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
+        val sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean("darkMode", false)
 
+        val darkModeSwitchPref = findPreference<SwitchPreferenceCompat>("darkModeSwitch")
+        darkModeSwitchPref?.isChecked = isDarkMode
+
+        darkModeSwitchPref?.setOnPreferenceChangeListener { preference: Preference, newValue: Any ->
+            val isDarkModeOn = newValue as Boolean
+            if (isDarkModeOn) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("darkMode", isDarkModeOn)
+            editor.apply()
+
+            true
+        }
     }
 }
