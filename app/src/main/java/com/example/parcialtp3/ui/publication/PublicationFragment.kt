65cb2f67d967a641577c39b9bf04f200ff5365f2
Matias Mandelbaum <105255748+mandelbaummatias@users.noreply.github.com>
2023-11-06 13:08:40 -0300

    Adding validations and handling errors for each field

--- app/src/main/java/com/example/parcialtp3/ui/publication/PublicationFragment.kt
+++ app/src/main/java/com/example/parcialtp3/ui/publication/PublicationFragment.kt
@@ -7,8 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.parcialtp3.R
@@ -40,50 +38,8 @@ class PublicationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getAllBreeds()
 
-
-        binding.buttonPublicar.setOnClickListener {
-            val breed = binding.dogBreed.text.toString()
-            val subBreed = binding.dogSubBreed.text.toString()
-
-            // Check if breed layout is visible and validate the breed
-            if (binding.dogBreedLayout.visibility == View.VISIBLE) {
-                viewModel.validateBreed(breed)
-
-                viewModel.isBreedValid.observe(viewLifecycleOwner) { isBreedValid ->
-                    if (!isBreedValid) {
-                        binding.dogBreedLayout.error = "La raza no puede estar vacia"
-                    } else {
-                        binding.dogBreedLayout.error = null
-                    }
-                }
-            }
-
-            // Check if sub-breed layout is visible and validate the sub-breed
-            if (binding.dogSubBreedLayout.visibility == View.VISIBLE) {
-                viewModel.validateSubBreed(subBreed)
-
-                viewModel.isSubBreedValid.observe(viewLifecycleOwner) { isSubBreedValid ->
-                    if (!isSubBreedValid) {
-                        binding.dogSubBreedLayout.error = "La subraza no puede estar vacia"
-                    } else {
-                        binding.dogSubBreedLayout.error = null
-                    }
-                }
-            }
-
-            // Proceed with form submission only if both breed and sub-breed are valid
-            if (binding.dogBreedLayout.visibility != View.VISIBLE || (viewModel.isBreedValid.value == true
-                        && (binding.dogSubBreedLayout.visibility != View.VISIBLE || viewModel.isSubBreedValid.value == true))
-            ) {
-                Log.d(TAG, "form is valid")
-                // Form is valid, proceed with submission
-                // You can move the submission logic here
-            }
-        }
-
+        initListeners()
         initObservers()
-    
-
     }
 
     private fun generateAdapter(): ArrayAdapter<String> {
@@ -92,7 +48,7 @@ class PublicationFragment : Fragment() {
     }
 
     private fun getFormattedEnumValues(enumClass: Class<out Enum<*>>): List<String> {
-        return enumClass.enumConstants.map { it ->
+        return enumClass.enumConstants.map {
             it.name.replace("_", " ").split(' ')
                 .joinToString(" ") { it.lowercase().titleCaseFirstChar() }
         }
@@ -114,8 +70,7 @@ class PublicationFragment : Fragment() {
         }
     }
 
-    private fun initObservers(){
-
+    private fun initObservers() {
         viewModel.breedsListState.observe(viewLifecycleOwner) {
             when (it) {
                 is Result.Success -> {
@@ -166,6 +121,161 @@ class PublicationFragment : Fragment() {
                     "",
                     false
                 ) // Clear the text without triggering the AutoComplete dropdown
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.buttonPublicar.setOnClickListener {
+            val breed = binding.dogBreed.text.toString()
+            val subBreed = binding.dogSubBreed.text.toString()
+            val dogName = binding.dogName.text.toString()
+            val dogAge = binding.dogAge.text.toString()
+            val dogGender = when (binding.dogGender.checkedRadioButtonId) {
+                R.id.male -> "Macho"
+                R.id.female -> "Hembra"
+                else -> ""
+            }
+            val dogDescription = binding.dogDescription.text.toString()
+            val dogWeight = binding.dogWeight.text.toString()
+            val dogLocation = binding.dogLocation.text.toString()
+
+            // Check if breed layout is visible and validate the breed
+            if (binding.dogBreedLayout.visibility == View.VISIBLE) {
+                viewModel.validateBreed(breed)
+
+                viewModel.isBreedValid.observe(viewLifecycleOwner) { isBreedValid ->
+                    if (!isBreedValid) {
+                        binding.dogBreedLayout.error = "La raza no puede estar vacia"
+                    } else {
+                        binding.dogBreedLayout.error = null
+                    }
+                }
+            }
+
+            // Check if sub-breed layout is visible and validate the sub-breed
+            if (binding.dogSubBreedLayout.visibility == View.VISIBLE) {
+                viewModel.validateSubBreed(subBreed)
+
+                viewModel.isSubBreedValid.observe(viewLifecycleOwner) { isSubBreedValid ->
+                    if (!isSubBreedValid) {
+                        binding.dogSubBreedLayout.error = "La subraza no puede estar vacia"
+                    } else {
+                        binding.dogSubBreedLayout.error = null
+                    }
+                }
+            }
+
+            viewModel.validateName(dogName) { isValid ->
+                if (!isValid) {
+                    binding.dogNameLayout.error = "El nombre no puede estar vacío"
+                } else {
+                    binding.dogNameLayout.error = null
+                }
+            }
+
+            viewModel.validateAge(dogAge) { isValid ->
+                if (!isValid) {
+                    binding.dogAgeLayout.error = "La edad no puede estar vacía"
+                } else {
+                    binding.dogAgeLayout.error = null
+                }
+            }
+
+            viewModel.validateGender(dogGender) { isValid ->
+                if (!isValid) {
+                    binding.dogGenderLayout.error = "Debes seleccionar un género para el perro"
+                } else {
+                    binding.dogGenderLayout.error = null
+                }
+            }
+
+            viewModel.validateDescription(dogDescription) { isValid ->
+                if (!isValid) {
+                    binding.dogDescriptionLayout.error = "La descripción no puede estar vacía"
+                } else {
+                    binding.dogDescriptionLayout.error = null
+                }
+            }
+
+            viewModel.validateWeight(dogWeight) { isValid ->
+                if (!isValid) {
+                    binding.dogWeightLayout.error = "El peso no puede estar vacío"
+                } else {
+                    binding.dogWeightLayout.error = null
+                }
+            }
+
+            viewModel.validateLocation(dogLocation) { isValid ->
+                if (!isValid) {
+                    binding.dogLocationLayout.error = "La ubicación no puede estar vacía"
+                } else {
+                    binding.dogLocationLayout.error = null
+                }
+            }
+
+
+            // Validate the rest of the fields
+//            viewModel.validateField(dogName) { isValid ->
+//                if (!isValid) {
+//                    binding.dogNameLayout.error = "El nombre no puede estar vacío"
+//                } else {
+//                    binding.dogNameLayout.error = null
+//                }
+//            }
+//
+//            viewModel.validateField(dogAge) { isValid ->
+//                if (!isValid) {
+//                    binding.dogAgeLayout.error = "La edad no puede estar vacía"
+//                } else {
+//                    binding.dogAgeLayout.error = null
+//                }
+//            }
+//
+//            viewModel.validateField(dogGender) { isValid ->
+//                if (!isValid) {
+//                    Toast.makeText(
+//                        requireContext(),
+//                        "Debes seleccionar un género para el perro",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//            }
+//
+//            viewModel.validateField(dogDescription) { isValid ->
+//                if (!isValid) {
+//                    binding.dogDescriptionLayout.error = "La descripción no puede estar vacía"
+//                } else {
+//                    binding.dogDescriptionLayout.error = null
+//                }
+//            }
+//
+//            viewModel.validateField(dogWeight) { isValid ->
+//                if (!isValid) {
+//                    binding.dogWeightLayout.error = "El peso no puede estar vacío"
+//                } else {
+//                    binding.dogWeightLayout.error = null
+//                }
+//            }
+//
+//            viewModel.validateField(dogLocation) { isValid ->
+//                if (!isValid) {
+//                    binding.dogLocationLayout.error = "La ubicación no puede estar vacía"
+//                } else {
+//                    binding.dogLocationLayout.error = null
+//                }
+//            }
+
+            // Proceed with form submission only if all fields are valid
+            if (binding.dogBreedLayout.visibility != View.VISIBLE ||
+                (viewModel.isBreedValid.value == true
+                        && (binding.dogSubBreedLayout.visibility != View.VISIBLE ||
+                        viewModel.isSubBreedValid.value == true)
+                        ) && viewModel.isFormValid.value == true
+            ) {
+                Log.d(TAG, "form is valid")
+                // Form is valid, proceed with submission
+                // You can move the submission logic here
             }
         }
     }
--- app/src/main/java/com/example/parcialtp3/ui/publication/PublicationViewModel.kt
+++ app/src/main/java/com/example/parcialtp3/ui/publication/PublicationViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.parcialtp3.ui.publication
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,6 +13,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+private const val TAG = "PublicationViewModel"
 @HiltViewModel
 class PublicationViewModel @Inject constructor(private val getBreedsFromApiUseCase: GetBreedsFromApiUseCase) :
     ViewModel() {
@@ -33,35 +36,120 @@ class PublicationViewModel @Inject constructor(private val getBreedsFromApiUseCa
     val isBreedValid: LiveData<Boolean>
         get() = _isBreedValid
 
+    private val _isSubBreedValid = MutableLiveData<Boolean>()
+    val isSubBreedValid: LiveData<Boolean>
+        get() = _isSubBreedValid
+
+    private val _isValidName = MutableLiveData<Boolean>()
+    val isValidName: LiveData<Boolean>
+        get() = _isValidName
+
+    private val _isValidAge = MutableLiveData<Boolean>()
+    val isValidAge: LiveData<Boolean>
+        get() = _isValidAge
+
+    private val _isValidGender = MutableLiveData<Boolean>()
+    val isValidGender: LiveData<Boolean>
+        get() = _isValidGender
+
+    private val _isValidDescription = MutableLiveData<Boolean>()
+    val isValidDescription: LiveData<Boolean>
+        get() = _isValidDescription
+
+    private val _isValidWeight = MutableLiveData<Boolean>()
+    val isValidWeight: LiveData<Boolean>
+        get() = _isValidWeight
+
+    private val _isValidLocation = MutableLiveData<Boolean>()
+    val isValidLocation: LiveData<Boolean>
+        get() = _isValidLocation
+
     init {
         _isFormValid.value = false
         _isBreedValid.value = false
+        _isSubBreedValid.value = true
+        _isValidName.value = false
+        _isValidAge.value = false
+        _isValidGender.value = false
+        _isValidDescription.value = false
+        _isValidWeight.value = false
+        _isValidLocation.value = false
     }
 
     fun validateBreed(breed: String) {
         _isBreedValid.value = breed.isNotEmpty()
         validateForm()
     }
-    private val _isSubBreedValid = MutableLiveData<Boolean>()
-    val isSubBreedValid: LiveData<Boolean>
-        get() = _isSubBreedValid
-
-    // Existing code...
 
     fun validateSubBreed(subBreed: String) {
         _isSubBreedValid.value = subBreed.isNotEmpty()
         validateForm()
     }
 
-    fun validateForm() {
-        _isFormValid.value = _isBreedValid.value == true /*&& other validation conditions*/
+    fun validateName(name: String, validationCallback: (Boolean) -> Unit) {
+        _isValidName.value = name.isNotEmpty()
+        val isValid = name.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
     }
 
+    fun validateAge(age: String, validationCallback: (Boolean) -> Unit) {
+        _isValidAge.value = age.isNotEmpty()
+        val isValid = age.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
+    }
+
+    fun validateGender(gender: String, validationCallback: (Boolean) -> Unit) {
+        _isValidGender.value = gender.isNotEmpty()
+        val isValid = gender.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
+    }
+
+    fun validateDescription(description: String, validationCallback: (Boolean) -> Unit) {
+        _isValidDescription.value = description.isNotEmpty()
+        val isValid = description.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
+    }
+
+    fun validateWeight(weight: String, validationCallback: (Boolean) -> Unit) {
+        _isValidWeight.value = weight.isNotEmpty()
+        val isValid = weight.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
+    }
+
+    fun validateLocation(location: String, validationCallback: (Boolean) -> Unit) {
+        _isValidLocation.value = location.isNotEmpty()
+        val isValid = location.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
+    }
+
+    fun validateForm() {
+        _isFormValid.value = _isBreedValid.value == true && _isSubBreedValid.value == true &&
+                _isValidName.value == true && _isValidAge.value == true &&
+                _isValidDescription.value == true && _isValidWeight.value == true && _isValidLocation.value == true
+        //&&_isValidGender.value == true
+        Log.d(TAG, "isFormValid ${isFormValid.value}")
+    }
+
+    fun validateField(field: String, validationCallback: (Boolean) -> Unit) {
+        val isValid = field.isNotEmpty()
+        validationCallback(isValid)
+        validateForm()
+    }
 
     fun setSelectedBreed(breed: String) {
         _selectedBreed.value = breed
     }
 
+    fun setSelectedGender(gender: String) {
+        _isValidGender.value = gender.isNotEmpty()
+        validateForm()
+    }
 
     fun getAllBreeds() {
         _breedsListState.postValue(Result.Loading)
@@ -73,7 +161,8 @@ class PublicationViewModel @Inject constructor(private val getBreedsFromApiUseCa
                 val breedsWithSubBreeds = breedList.map { (breedName, subBreeds) ->
                     BreedWithSubBreeds(
                         breedName.titleCaseFirstChar(),
-                        subBreeds.map { it.titleCaseFirstChar() })
+                        subBreeds.map { it.titleCaseFirstChar() }
+                    )
                 }
                 _breedsList.postValue(breedsWithSubBreeds)
             }
